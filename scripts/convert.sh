b196123c30fdf4008158292e4527876a6709591a
lukas-heiligenbrunner <lukas.heiligenbrunner@gmail.com>
2019-01-26 09:22:09 +0100

    deleted current video file

--- scripts/convert.sh
+++ scripts/convert.sh
@@ -4,9 +4,6 @@ echo "" > finished.txt
 
 for file in ../videos/*.ts
 do
-  echo $file > currentvid.info
-
-
   echo "" > output.txt
   ffmpeg -i "$file" -threads 2 -y "${file%.ts}".mp4 &> output.txt
   echo $file >> finished.txt
