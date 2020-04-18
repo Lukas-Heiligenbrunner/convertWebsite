bcc6395ef5a793c1d2df00a47db4f3d0ce31abb2
root <root@konvertieren.local>
2020-04-18 12:37:09 +0000

    edited bash scripts

---
+++ php/convert.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "" > finished.txt
+
+for file in ../videos/*.ts
+do
+  echo "42" > output.txt
+  ffmpeg -i "$file" -threads 3 -y "${file%.ts}".mp4 &> output.txt
+  echo $file >> finished.txt
+done
+
+rm output.txt
